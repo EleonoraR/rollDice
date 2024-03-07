769636b25642fec1143089fde909bce1d73c709b
Eleonora <61467087+EleonoraR@users.noreply.github.com>
2024-03-06 20:42:51 -0500

    Uploaded index.html, styles.css and script.js
    Also 6 images of dices(1-6 dices)

---
+++ script.js
@@ -0,0 +1,31 @@
+function rollDice() {
+  var randomDice = Math.floor(Math.random() * 6) + 1;
+
+  var randomImgSource = 'img/dice' + randomDice + '.png';
+
+  var randomImg = document.querySelectorAll('img')[0];
+  randomImg.setAttribute('src', randomImgSource);
+
+  //----------------------------------------------------------
+  var randomDice2 = Math.floor(Math.random() * 6) + 1;
+
+  var randomImgSource2 = 'img/dice' + randomDice2 + '.png';
+
+  var randomImg2 = document.querySelectorAll('img')[1];
+  randomImg2.setAttribute('src', randomImgSource2);
+
+  //----------------------------------------------------------
+  if (randomDice > randomDice2) {
+    document.querySelector('.title').innerHTML = 'Player 1 won!!!';
+  } else if (randomDice < randomDice2) {
+    document.querySelector('.title').innerHTML = 'Player 2 won!!!';
+  } else {
+    document.querySelector('.title').innerHTML = 'Draw!!!';
+  }
+}
+
+//BUTTON-----------------------------------------
+var buttonPlay = document.getElementById('btn');
+console.log(buttonPlay);
+
+buttonPlay.addEventListener('click', rollDice);
